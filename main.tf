0b77da1790c3da9d4dc2dbf3e9db12ef2c3baf43
Akshay Joseph <kmjakshay@gmail.com>
2023-01-05 12:24:42 +0100

    Added Loadbalancer

--- main.tf
+++ main.tf
@@ -6,13 +6,13 @@ resource "aws_vpc" "this" {
   # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc#enable_dns_hostnames
   enable_dns_hostnames = true
   tags = {
-    "Name" = "Application-1"
+    "Name" = "Application-1-vnet"
   }
 }
 resource "aws_subnet" "private-2a" {
   vpc_id            = aws_vpc.this.id
   cidr_block        = "10.20.20.0/28"
-  availability_zone = "us-east-2a"
+  availability_zone = "us-west-2a"
   tags = {
     "Name" = "Application-1-private-2a"
   }
@@ -20,7 +20,7 @@ resource "aws_subnet" "private-2a" {
 resource "aws_subnet" "private-2b" {
   vpc_id            = aws_vpc.this.id
   cidr_block        = "10.20.20.16/28"
-  availability_zone = "us-east-2b"
+  availability_zone = "us-west-2b"
   tags = {
     "Name" = "Application-1-private-2b"
   }
@@ -28,7 +28,7 @@ resource "aws_subnet" "private-2b" {
 resource "aws_subnet" "private-2c" {
   vpc_id            = aws_vpc.this.id
   cidr_block        = "10.20.20.32/28"
-  availability_zone = "us-east-2c"
+  availability_zone = "us-west-2c"
   tags = {
     "Name" = "Application-1-private-2c"
   }
